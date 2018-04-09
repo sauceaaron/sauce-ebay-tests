9da24825eff7043ed238cac8c557616d7d419cef
sauceaaron <aaron.evans@saucelabs.com>
2018-04-09 12:17:24 -0600

    use dynamic SearchPage

--- src/main/java/SearchPage.java
+++ src/main/java/SearchPage.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class SearchPage
 {
@@ -9,7 +10,43 @@ public class SearchPage
 
 	public static class Categories {
 		public static int ToysAndHobbies = 32;
-	};
+	}
 
 	public static By searchButton = By.id("gh-btn");
+
+	private WebDriver driver;
+
+	public static SearchPage Open(WebDriver driver)
+	{
+		return new SearchPage(driver).load();
+	}
+
+	public SearchPage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+
+	public SearchPage load()
+	{
+		driver.get(url);
+		return this;
+	}
+
+	public SearchPage enterSearchText(String text)
+	{
+		driver.findElement(searchBox).sendKeys(text);
+		return this;
+	}
+
+	public SearchPage selectCategory(int category)
+	{
+		driver.findElement(categoriesDropDown).findElements(By.tagName("option")).get(category);
+		return this;
+	}
+
+	public SearchPage clickSearchButton()
+	{
+		driver.findElement(searchButton).click();
+		return this;
+	}
 }
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,13 +1,13 @@
 import org.junit.Test;
-import org.openqa.selenium.By;
 public class SearchTest extends SauceTestBase
 {
 	@Test
 	public void should_find_an_item()
 	{
-		driver.get(SearchPage.url);
-		driver.findElement(SearchPage.searchBox).sendKeys("rocket");
-		driver.findElement(SearchPage.categoriesDropDown).findElements(By.tagName("option")).get(SearchPage.Categories.ToysAndHobbies).click();
-		driver.findElement(SearchPage.searchButton).click();
+		SearchPage
+				.Open(driver)
+				.enterSearchText("rocket")
+				.selectCategory(SearchPage.Categories.ToysAndHobbies)
+				.clickSearchButton();
 	}
 }
